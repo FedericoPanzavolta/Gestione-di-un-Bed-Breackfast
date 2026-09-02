3c5522b44d43f22ffc40b295725e5d8fedd05e18
Federico Panzavolta <federico.panzavolta@studio.unibo.it>
2026-09-02 16:38:18 +0200

    Aggiunta applicazione 'BedAndBreakfast' nell'elenco delle applicazioni in 'BnB/settings.py'

--- BnB/settings.py
+++ BnB/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "BedAndBreakfast.apps.BedandbreackfastConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
